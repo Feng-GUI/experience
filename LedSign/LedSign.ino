fc9de37bc4634d6839bfed0ee8679e91a996a160
rafaelmizrahi <rafaelmizrahi@gmail.com>
2016-03-21 23:13:56 +0200

    multiple ws2812. does not work. could not get ALA to run few WS2811 strips.

--- LedSign/LedSign.ino
+++ LedSign/LedSign.ino
@@ -3,10 +3,21 @@
 // Leds sign has 4 WS2811 strips
 // each strip receives a sequence of animations that runs in paralel.
 
-// Arduino Diecimila and Uno PWM: 3, 5, 6, 9, 10, and 11. 
-//
+// Arduino Diecimila and Uno PWM pins: 3, 5, 6, 9, 10, and 11. 
+// WS2812 strip pins
+#define STRIP1_PWM 3
+#define STRIP2_PWM 5
+#define STRIP3_PWM 6
+#define STRIP4_PWM 9
 
+// potentiometer input
+#define POTENTIOMETER_PIN 2
 
+// Set the number of pixel in each strip
+#define STRIP1_NUM_PIXELS 60
+#define STRIP2_NUM_PIXELS 20
+#define STRIP3_NUM_PIXELS 40
+#define STRIP4_NUM_PIXELS 25
 
 /////////////////////////////////////////////////////////
 //
@@ -69,6 +80,8 @@
 
 AlaLedRgb strip1;
 AlaLedRgb strip2;
+AlaLedRgb strip3;
+AlaLedRgb strip4;
 
 // custom palette :         black     white     black     red       green     blue
 AlaColor mycolors_[6] = { 0x000000, 0xFFFFFF, 0x000000, 0xFF0000, 0x00FF00, 0x0000FF };
@@ -80,39 +93,29 @@ AlaSeq seq1[] = {
   { ALA_OFF, 1000, 1000, alaPalNull },
   { ALA_FADECOLORSLOOP, 4000, 8000, alaPalRgb },
   { ALA_FADECOLORSLOOP, 500, 4000, mycolors },
-  { ALA_OFF, 1000, 1000, alaPalNull },
   { ALA_FADECOLORS, 5000, 5000, mycolors },
-  { ALA_OFF, 1000, 1000, alaPalNull },
   { ALA_ENDSEQ }
 };
 
 AlaSeq seq2[] = {
   { ALA_CYCLECOLORS, 3000, 3000, alaPalRgb },
   { ALA_OFF, 1000, 1000, alaPalNull },
-  { ALA_FADECOLORSLOOP, 4000, 8000, alaPalRgb },
-  { ALA_FADECOLORSLOOP, 500, 4000, mycolors },
-  { ALA_OFF, 1000, 1000, alaPalNull },
-  { ALA_FADECOLORS, 5000, 5000, mycolors },
+  { ALA_PIXELSMOOTHBOUNCE, 4000, 8000, alaPalRgb },
+  { ALA_FADECOLORS, 4000, 4000, mycolors },
   { ALA_OFF, 1000, 1000, alaPalNull },
   { ALA_ENDSEQ }
 };
 
 AlaSeq seq3[] = {
-  { ALA_CYCLECOLORS, 3000, 3000, alaPalRgb },
-  { ALA_OFF, 1000, 1000, alaPalNull },
-  { ALA_FADECOLORSLOOP, 4000, 8000, alaPalRgb },
-  { ALA_FADECOLORSLOOP, 500, 4000, mycolors },
-  { ALA_OFF, 1000, 1000, alaPalNull },
-  { ALA_FADECOLORS, 5000, 5000, mycolors },
-  { ALA_OFF, 1000, 1000, alaPalNull },
+  { ALA_BLINKALT, 4000, 8000, alaPalRgb },
   { ALA_ENDSEQ }
 };
 
 AlaSeq seq4[] = {
-  { ALA_CYCLECOLORS, 3000, 3000, alaPalRgb },
+  { ALA_PIXELSMOOTHBOUNCE, 3000, 3000, alaPalRgb },
   { ALA_OFF, 1000, 1000, alaPalNull },
   { ALA_FADECOLORSLOOP, 4000, 8000, alaPalRgb },
-  { ALA_FADECOLORSLOOP, 500, 4000, mycolors },
+  { ALA_CYCLECOLORS, 500, 4000, mycolors },
   { ALA_OFF, 1000, 1000, alaPalNull },
   { ALA_FADECOLORS, 5000, 5000, mycolors },
   { ALA_OFF, 1000, 1000, alaPalNull },
@@ -121,29 +124,44 @@ AlaSeq seq4[] = {
 
 void setup()
 {
-  strip1.initWS2812(8, 2);
-  strip2.initWS2812(8, 2);
-  strip3.initWS2812(8, 2);
-  strip4.initWS2812(8, 2);
-      
+  // init strips PWM pins
+  // initWS2812(int numLeds, byte pin, byte type)
+  // default type = NEO_GRB + NEO_KHZ800 = 0x01 + 0x02
+  strip1.initWS2812(STRIP1_NUM_PIXELS, STRIP1_PWM);
+  strip2.initWS2812(STRIP2_NUM_PIXELS, STRIP2_PWM);
+  //strip3.initWS2812(STRIP3_NUM_PIXELS, STRIP3_PWM);
+  //strip4.initWS2812(STRIP4_NUM_PIXELS, STRIP4_PWM);
+  
+  // set strip sequences
   strip1.setAnimation(seq1);
   strip2.setAnimation(seq2);
-  strip3.setAnimation(seq3);
-  strip4.setAnimation(seq4);
+  //strip3.setAnimation(seq3);
+  //strip4.setAnimation(seq4);
 }
 
 void loop()
 {
+  // check potentiometer and set brightness
+  //setBrightness();
+  
+  // advance animations of all strips
   strip1.runAnimation();
+  strip2.runAnimation();
+  //strip3.runAnimation();
+  //strip4.runAnimation();
 }
 
+// check potentiometer and set brightness of all strips
 void setBrightness()
 {
-  // FIXME: get potentiometer value 
+  // read potentiometer value 
+  int potVal = analogRead(POTENTIOMETER_PIN);
+  // map potentiometer value to brightness value
+  int brightVal = map(potVal, 0, 1023, 0, 254);
   
-  strip1.setBrightness();
-  strip2.setBrightness();
-  strip3.setBrightness();
-  strip4.setBrightness();
+  strip1.setBrightness(brightVal);
+  strip2.setBrightness(brightVal);
+  strip3.setBrightness(brightVal);
+  strip4.setBrightness(brightVal);
 }
 
