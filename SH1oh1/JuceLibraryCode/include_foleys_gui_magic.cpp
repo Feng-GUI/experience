c003b6c0353fcb0e139fc13955785f77c580c905
rafaelmizrahi <rafaelmizrahi@gmail.com>
2023-09-25 18:42:15 +0300

    SH1oh1

---
+++ SH1oh1/JuceLibraryCode/include_foleys_gui_magic.cpp
@@ -0,0 +1,8 @@
+/*
+
+    IMPORTANT! This file is auto-generated each time you save your
+    project - if you alter its contents, your changes may be overwritten!
+
+*/
+
+#include <foleys_gui_magic/foleys_gui_magic.cpp>
