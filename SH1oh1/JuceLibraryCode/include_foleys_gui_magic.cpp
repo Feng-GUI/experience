9b3037d20b244aca92465976d5659c6910f4957a
Rafael Mizrahi <rafaelmizrahi@gmail.com>
2023-09-27 18:52:02 +0300

    Delete SH1oh1 directory

--- SH1oh1/JuceLibraryCode/include_foleys_gui_magic.cpp
+++
@@ -1,8 +0,0 @@
-/*
-
-    IMPORTANT! This file is auto-generated each time you save your
-    project - if you alter its contents, your changes may be overwritten!
-
-*/
-
-#include <foleys_gui_magic/foleys_gui_magic.cpp>
